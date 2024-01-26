0dc55b005afb96d0a9e3932649356511a8b3bcfa
add0794 <add0794@gmail.com>
2024-01-25 22:08:15 -0500

    remove unnecessary comments in CSS file and change variable in TypeScript file

--- index.ts
+++ index.ts
@@ -1,30 +1,32 @@
-var btn = document.getElementById("btn");
-var birthday = document.getElementById("day");
-var resultParagraph = document.getElementsByClassName("result")[0]; // Access the first element in the collection
-btn.addEventListener("click", function () {
-    var birthdayValue = document.getElementById('day').value;
-    var birthdayDate = new Date(birthdayValue);
-    var today = new Date();
+const btn = document.getElementById("btn") as HTMLButtonElement;
+const birthday = document.getElementById("day") as HTMLInputElement;
+const resultParagraph = document.getElementsByClassName("result")[0] as HTMLParagraphElement; // Access the first element in the collection
+
+btn.addEventListener("click", (): void => {
+    const birthdayValue: string = (document.getElementById('day') as HTMLInputElement).value;
+    const birthdayDate: Date = new Date(birthdayValue);
+    const today: Date = new Date();
+
     if (birthdayValue === "") {
         alert("Please enter your birthday.");
-    }
-    else {
-        var age = void 0;
+    } else {
+        let age: number;
+
         if (today.getMonth() > birthdayDate.getMonth() ||
             (today.getMonth() === birthdayDate.getMonth() && today.getDate() >= birthdayDate.getDate())) {
             // Birthday has occurred this year or it's today
             age = today.getFullYear() - birthdayDate.getFullYear();
-            document.getElementById('resultParagraph').textContent = "You are ".concat(age, " years old.");
-            alert("You are ".concat(age, " years old."));
-        }
-        else {
+            (document.getElementById('resultParagraph') as HTMLParagraphElement).textContent = `You are ${age} years old.`;
+            alert(`You are ${age} years old.`);
+        } else {
             // Birthday hasn't occurred yet this year
             age = today.getFullYear() - birthdayDate.getFullYear() - 1;
-            resultParagraph.textContent = "You are ".concat(age, " years old.");
-            alert("You are ".concat(age, " years old."));
+            resultParagraph.textContent = `You are ${age} years old.`;
+            alert(`You are ${age} years old.`);
         }
     }
-});
+})
+
 /* Option 2
 
 function calculateAge(): void {
@@ -52,4 +54,4 @@ function calculateAge(): void {
     }
 }
 
-btn.addEventListener("click", calculateAge); */ 
+btn.addEventListener("click", calculateAge); */
